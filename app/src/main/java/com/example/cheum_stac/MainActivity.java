396fd0208a28618804830a04eaf42728f3d5ad59
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-08-06 03:44:41 +0900

    bottom tab

--- app/src/main/java/com/example/cheum_stac/MainActivity.java
+++ app/src/main/java/com/example/cheum_stac/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.cheum_stac;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private HomeFragment homeFragment = new HomeFragment();
+    private MapFragment mapFragment = new MapFragment();
+    private KategorieFragment kategorieFragment = new KategorieFragment();
+    private MyPageFragment myPageFragment = new MyPageFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, homeFragment).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.btn_navi_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
     }
+
+     class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+         @Override
+         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+             FragmentTransaction transaction = fragmentManager.beginTransaction();
+             switch (item.getItemId()){
+                 case R.id.tab_home:
+                     transaction.replace(R.id.frameLayout,homeFragment).commitAllowingStateLoss();
+                     break;
+                 case R.id.tab_map:
+                     transaction.replace(R.id.frameLayout,mapFragment).commitAllowingStateLoss();
+                     break;
+                 case R.id.tab_kate:
+                     transaction.replace(R.id.frameLayout,kategorieFragment).commitAllowingStateLoss();
+                     break;
+                 case R.id.tab_my:
+                     transaction.replace(R.id.frameLayout,myPageFragment).commitAllowingStateLoss();
+                     break;
+             }
+             return false;
+         }
+     }
 }
