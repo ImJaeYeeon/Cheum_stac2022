4fffaf4b9c1efea339a80646bbc60f31891dc593
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-08-12 01:36:10 +0900

    theme change

--- app/src/main/java/com/example/cheum_stac/HomeFragment.java
+++ app/src/main/java/com/example/cheum_stac/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.cheum_stac;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,7 +60,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
